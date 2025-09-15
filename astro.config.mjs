e35f2d3b86d96e291f968f8f6b350497d73ecce3
Seungwoo Han <seungwoohan0108@gmail.com>
2025-09-16 01:27:28 +0900

    Update astro.config.mjs

--- astro.config.mjs
+++ astro.config.mjs
@@ -26,8 +26,8 @@ import { pluginCustomCopyButton } from "./src/plugins/expressive-code/custom-cop
 
 // https://astro.build/config
 export default defineConfig({
-	site: "https://kaintels.github.io/blog",
-	base: "/",
+	site: "https://kaintels.github.io",
+	base: "/blog",
 	trailingSlash: "always",
 	integrations: [
 		tailwind({
